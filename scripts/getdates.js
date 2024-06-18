47e4aa41c72e7ae157a90b2d76c0226dea81e666
uronico <uronico3@gmail.com>
2024-06-19 04:17:12 +0800

    save progress

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+<script>
+        // Populate current year
+    document.getElementById("currentyear").textContent = new Date().getFullYear();
+    // Populate last modified date
+    document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+</script>
